09bf0e2dda5f7a89379ec5ca21b8a2813a3aad25
Tilo Klarenbeek <tiloklarenbeek@gmail.com>
2023-04-24 17:13:15 +0200

    Added image

--- example.py
+++ example.py
@@ -1,17 +1,23 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtGui import QPixmap
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
         self.width = 300
-        self.height = 300
+        self.height = 600
         self.initUI()
 
     def initUI(self):
         # Create a label
         self.lbl = QLabel("PyQt5 Example App", self)
+
+        # Add an image
+        lbl_image = QLabel(self)
+        pixmap = QPixmap("python.png").scaled(self.width, self.width)
+        lbl_image.setPixmap(pixmap)
 
         # Create a button
         btn = QPushButton("Click me!", self)
@@ -19,6 +25,7 @@ class Example(QWidget):
 
         # Create a vertical layout and add the label and button
         vbox = QVBoxLayout()
+        vbox.addWidget(lbl_image)
         vbox.addWidget(self.lbl)
         vbox.addWidget(btn)
 
@@ -26,7 +33,7 @@ class Example(QWidget):
         self.setLayout(vbox)
 
         # Set the window size
-        self.setGeometry(self.width, self.height, 250, 150)
+        self.setGeometry(100, 100, self.width, self.height)
         self.setWindowTitle("PyQt5 Example")
         self.show()
 
